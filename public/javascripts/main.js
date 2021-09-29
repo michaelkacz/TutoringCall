e070b5a3bd9aefdd23f89522c73aafe15b3279fa
Michael Kaczowka <mkaczowka@hawk.iit.edu>
2021-09-29 11:41:48 -0500

    Fix typos and add socket server events/callbacks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,14 +3,14 @@
 //variable '$self' is used for things on users side of call
 //starts by setting audio to off and video to on
 const $self = {
-  rtcConfig: null;
+  rtcConfig: null,
   constraints: { audio: false, video: true }
 };
 
 //$peer object is used as the second person in syscal
 //Sets up a connection between two people ($self and $peer)
-const $peer {
-  connection: new RTCPeerConnection($self.rtcConfig);
+const $peer = {
+  connection: new RTCPeerConnection($self.rtcConfig)
 };
 
 requestUserMedia($self.constraints);
@@ -18,8 +18,14 @@ requestUserMedia($self.constraints);
 //requests media usage from user in pop up
 //finding video instance and setting video object to $self stream
 async function requestUserMedia(constraints) {
-  cont video = document.querySelector('$self');
+  const video = document.querySelector('$self');
   $self.stream = await navigator.mediaDevices
     .getUserMedia(constraints);
   video.srcObject = $self.stream;
 }
+
+//Socket server events and callbacks
+const sc = io();
+sc.on('connect', function() {
+  console.log('Socket.io connection established');
+});
